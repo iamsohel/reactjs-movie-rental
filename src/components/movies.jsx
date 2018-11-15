eeb0ad380a66665d6119908a68a6984de185cf1a
Sohel Rana <srana@leapinglogic.com>
2018-11-15 18:45:41 +0600

    table added

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import { getMovies } from '../services/fakeMovieService'
+
 class Movies extends Component {
-    state = {  }
+    state = {  
+        movies : getMovies()
+    };
+
+    handleDelele = movie => {
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies});
+    }
     render() { 
+        if(this.state.movies.length === 0) return <p> there is no movie in database.</p>;
         return (
+            <React.Fragment>
+                <p>showing {this.state.movies.length} movie in the database</p>
             <table className="table table-bordered">
                 <thead>
                     <tr>
@@ -10,18 +22,24 @@ class Movies extends Component {
                         <th>Genre</th>
                         <th>Stock</th>
                         <th>Rate</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>afds</td>
-                        <td>asdf</td>
-                        <td>asdf</td>
-                        <td>asf</td>
+                    {this.state.movies.map( movie => (
+                    <tr key ={movie._id}>
+                        <td>{movie.title}</td>
+                        <td>{movie.genre.name}</td>
+                        <td>{movie.numberInStock}</td>
+                        <td>{movie.dailyRentalRate}</td>
+                        <td><button onClick={() => this.handleDelele(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                     </tr>
+                    )
+                    )}
                 </tbody>
             </table>
-              );
+            </React.Fragment>
+        );
     }
 }
  

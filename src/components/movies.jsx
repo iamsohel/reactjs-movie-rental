e4dd440410952da9f0ee5e3428b2b01e8d4568fa
sohel <sohelcuetcse11@gmail.com>
2018-11-15 09:38:09 +0600

    first commit

---
+++ src/components/movies.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+class Movies extends Component {
+    state = {  }
+    render() { 
+        return (
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Genre</th>
+                        <th>Stock</th>
+                        <th>Rate</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>afds</td>
+                        <td>asdf</td>
+                        <td>asdf</td>
+                        <td>asf</td>
+                    </tr>
+                </tbody>
+            </table>
+              );
+    }
+}
+ 
+export default Movies;

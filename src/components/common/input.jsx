420d532234fb03fce2c19922bc6fc87aaf27f8c4
Sohel Rana <srana@leapinglogic.com>
2018-12-12 12:18:33 +0600

    initial comm

---
+++ src/components/common/input.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+const Input = ({name, label, error, ...rest}) => {
+    return (  
+        <div className="form-group">
+            <label htmlFor={name}>{label}</label>
+            <input {...rest}
+                name={name}
+                className="form-control" 
+                id={name} 
+                placeholder=""/>
+               { error &&  <div  className="alert alert-danger">{error}</div>}
+        </div>
+    );
+};
+ 
+export default Input;
